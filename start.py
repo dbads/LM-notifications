bfdf660ca264ec2fe7f1b81310f27dc18040004d
Deepak Bharti <deepak.bharti@collegedunia.com>
2021-04-03 20:03:28 +0530

    app starter, listens for commands and process them

--- start.py
+++ start.py
@@ -1,4 +1,8 @@
+import sys
+import traceback
 from User import User
+from Topic import Topic
+
 
 def start():
   while True:
@@ -7,14 +11,48 @@ def start():
     [command_name, arg1, arg2] = command.split(' ')
     print(command_name, arg1, arg2)
 
-    if command_name == 'addUser':
-      users = User({})
-      users.add_user({'role': arg2, 'username': arg1})
-    else:
-      print('please input a valid command')
+    users = User()
+    topics = Topic()
+
+    try:
+      # user actions
+      if command_name == 'addUser':
+        users.add_user({'role': arg2, 'username': arg1})
+      elif command_name == 'removeUser':
+        users.add_user({'from_user': arg2, 'to_user': arg1})
+
+      # topic actions
+      elif command_name == 'addTopic':
+        topics.add_topic({'username': arg1, 'topicname': arg2, 'users': users})
+      elif command_name == 'removeTopic':
+        topics.remove_topic({'username': arg1, 'topicname': arg2, 'users': users})
+
+      elif command_name == 'resetUsers':
+        users.reset()
+      else:
+        print('please input a valid command')
+    except BaseException as ex:
+      # Get current system exception
+      ex_type, ex_value, ex_traceback = sys.exc_info()
+
+      # Extract unformatter stack traces as tuples
+      trace_back = traceback.extract_tb(ex_traceback)
+
+      # Format stacktrace
+      stack_trace = list()
+
+      for trace in trace_back:
+        stack_trace.append("File : %s , Line : %d, Func.Name : %s, Message : %s" %
+                           (trace[0], trace[1], trace[2], trace[3]))
+
+        print("Exception type : %s " % ex_type.__name__)
+        print("Exception message : %s" % ex_value)
+        print("Stack trace : %s" % stack_trace)
 
     print('users in the system - ')
     print(users.users)
+    print('topics in the system - ')
+    print(topics.topics)
 
 
 start()
